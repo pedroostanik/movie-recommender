040a2591f689fd80a2412671d5e57c6a799f89dd
Pedro Ostanik <pedro.ostanik@join4.com.br>
2024-04-25 18:12:55 -0300

    logging

--- data/database/my_data_base.py
+++ data/database/my_data_base.py
@@ -1,14 +1,20 @@
 from sqlalchemy import create_engine
 import pandas as pd
 import os
+import logging
 
 # 179.225.130.214
 
 class MyDataBase():    
 
     def __init__(self):   
+        logging.basicConfig(level=logging.DEBUG)
+        
         DB_USER = os.environ.get('DB_USER')
         DB_PASSWORD = os.environ.get('DB_PASSWORD')
+
+        logging.debug(f'DB_USER: {DB_USER}')
+        logging.debug(f'DB_PASSWORD: {DB_PASSWORD}')
         DB_HOST = "34.123.119.20" 
         DB = "app-recomendacao" 
         DB_CONNECTION = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB}'
