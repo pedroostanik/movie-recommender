58a44ea93dc3b4b77406a1ebfcee4265fbbaa5cc
Pedro Ostanik <pedro.ostanik@join4.com.br>
2024-04-26 00:12:13 -0300

    print

--- data/database/my_data_base.py
+++ data/database/my_data_base.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 import os
 import logging
+import socket
 
 # 179.225.130.214
 
@@ -9,6 +10,13 @@ class MyDataBase():
 
     def __init__(self):   
         logging.basicConfig(level=logging.DEBUG)
+
+         # Obtém o nome do host
+        hostname = socket.gethostname()
+        # Obtém o endereço IP associado ao nome do host
+        local_ip = socket.gethostbyname(hostname)
+        print(f'LOCAL IP:{local_ip}') 
+        logging.debug(f'LOCAL IP: {local_ip}')
 
         DB_USER = os.environ.get('DB_USER')
         DB_PASSWORD = os.environ.get('DB_PASSWORD')
